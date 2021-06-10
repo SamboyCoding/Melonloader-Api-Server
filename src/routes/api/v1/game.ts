8c794690c9756d128e481b9160dc76f23af94aef
Sam Byass <sam.byass@warwick.ac.uk>
2021-06-10 12:24:23 +0100

    Add patch endpoints for unhollower + dumper version

--- src/routes/api/v1/game.ts
+++ src/routes/api/v1/game.ts
@@ -105,4 +105,59 @@ apiV1GameRouter.patch("/:gameSlug/obfuscationRegex", async (req, res) => {
     return res.status(204).send();
 });
 
+apiV1GameRouter.patch("/:gameSlug/unhollowerVersion", async (req, res) => {
+    if(!req.session || !req.session.user)
+        return res.status(401).json({error: "X-SESSION-ID header is missing or invalid"});
+
+    if(req.session.user.privilegeLevel === 'editor')
+        return res.status(403).json({error: "You do not have permission to edit unhollower versions."});
+
+    const game = await Game.findOne(req.params.gameSlug);
+
+    if(!game)
+        return res.status(404).json({error: `No game found for game slug: ${req.params.gameSlug}`});
+
+    if(req.body.unhollowerVersion === undefined)
+        return res.status(400).json({error: "Missing new unhollowerVersion"});
+
+    const newVersion = req.body.unhollowerVersion;
+
+    if(newVersion !== game.forceUnhollowerVersion) {
+        console.log(`[Game] ${req.session.user.username} updated the unhollower version for ${game.gameName} from "${game.forceUnhollowerVersion}" to "${newVersion}"`);
+
+        game.forceUnhollowerVersion = newVersion ? newVersion : null;
+        await game.save();
+    }
+
+    return res.status(204).send();
+});
+
+apiV1GameRouter.patch("/:gameSlug/dumperVersion", async (req, res) => {
+    if(!req.session || !req.session.user)
+        return res.status(401).json({error: "X-SESSION-ID header is missing or invalid"});
+
+    if(req.session.user.privilegeLevel === 'editor')
+        return res.status(403).json({error: "You do not have permission to edit dumper versions."});
+
+    const game = await Game.findOne(req.params.gameSlug);
+
+    if(!game)
+        return res.status(404).json({error: `No game found for game slug: ${req.params.gameSlug}`});
+
+    if(req.body.dumperVersion === undefined)
+        return res.status(400).json({error: "Missing new dumperVersion"});
+
+    const newVersion = req.body.dumperVersion;
+
+    if(newVersion !== game.forceCpp2IlVersion) {
+        console.log(`[Game] ${req.session.user.username} updated the dumper version for ${game.gameName} from "${game.forceCpp2IlVersion}" to "${newVersion}"`);
+
+        game.forceCpp2IlVersion = newVersion ? newVersion : null;
+        await game.save();
+    }
+
+    return res.status(204).send();
+});
+
+
 export default apiV1GameRouter;
